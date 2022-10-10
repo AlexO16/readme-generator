a932b807ea2f088850e42be4d42077e2bbf3404b
Alex Oleynik <alexoleynik74@gmail.com>
2022-10-10 11:12:12 -0500

    updated code

--- index.js
+++ index.js
@@ -1,15 +1,94 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of the project?',
+    'Provide a description of the project:',
+    'How do you install this project?',
+    'How do you use this project?',
+    'How should people contribute to this project?',
+    'How do you test this project?',
+    'Choose a license for this project:',
+    'Enter your GitHub username:',
+    'Enter your Email:',
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile('README.md', data, (err) => 
+    err ? console.log("error") : console.log("README.md created!"))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'use',
+        },
+        {
+            type:  'input',
+            message: questions[4],
+            name: 'contribute',
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'test',
+        },
+        {
+            type: 'list',
+            message: questions[6],
+            choices: ['MIT', 'ISC', 'Apache', 'ODbl', 'None'],
+            name: 'license',
+            suffix: "press enter to select",
+        },
+        {
+           type: 'input',
+           message: questions[7],
+           name: 'github', 
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'email'
+        }
+        
+    ])
+
+    .then((data) => {
+        // console.log(data.title)
+        // console.log(data.description)
+        // console.log(data.install)
+        // console.log(data.use)
+        // console.log(data.contribute)
+        // console.log(data.test)
+        // console.log(data.license)
+        // console.log(data.github)
+        // console.log(data.email)
+        writeToFile(generateMarkdown(data));
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,124 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (license === 'ISC') {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+  } else if (license === 'Apache') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else if (license === 'ODbl') {
+    return `[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)`
+  } else {
+    return ''
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `[MIT](https://opensource.org/licenses/MIT)`
+  } else if (license === 'ISC') {
+    return `[ISC](https://opensource.org/licenses/ISC)`
+  } else if (license === 'Apache') {
+    return `[Apache](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else if (license === 'ODbl') {
+    return `[ODbl](https://opendatacommons.org/licenses/odbl/)`
+  } else {
+    return ''
+  }
+};
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license, githubUser) {
+  
+  const date = new Date();
+  let year = date.getFullYear();
+
+  let licenseSection;
+  switch (license) {
+    case 'MIT':
+      licenseSection = `Copyright ${year} ${githubUser}
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        
+      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+      break;
+    case 'ISC':
+      licenseSection = `Copyright ${year} ${githubUser}
+
+      Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+      
+      THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`
+      break;
+
+    case 'Apache':
+      licenseSection = `Copyright ${year} ${githubUser}
+
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+   
+        http://www.apache.org/licenses/LICENSE-2.0
+   
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.`
+      break;
+    default:
+      licenseSection = `No license selected`
+  }
+  return licenseSection
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `${renderLicenseBadge(data.license)}
+  # ${data.title}
+  
+  # Description
+  ${data.description}
 
-`;
+  # Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Testing](#testing)
+  * [License](#license)
+  * [Questions](#questions)
+
+  # Installation
+  ${data.install}
+
+  # Usage 
+  ${data.use}
+
+  # Contribution
+  ${data.contribute}
+
+  # Testing
+  ${data.test}
+
+  # License
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license, data.github)}
+
+  # Questions
+  GitHub username: [${data.github}](https://github.com/${data.github})
+
+
+
+  `;
 }
 
 module.exports = generateMarkdown;
